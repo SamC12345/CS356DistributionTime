408e7ad2c363f9ca17d62277449e483491251ba0
Sam <student979@live.com>
2021-10-10 23:32:00 -0400

    add graph

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,32 +6,33 @@ import {
   Link,
   VStack,
   Code,
+  Heading,
+  Spacer,
   Grid,
   theme,
+  HStack,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import { ClientServerGraph } from './ClientServerGraph';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+          <HStack>
+            <VStack justifySelf="flex-center" spacing={0}>
+              <Spacer />
+              <Heading>
+                Distribution Time for Client-Server and P2P Architectures
+              </Heading>
+              <Text>Samuel Carlos CS356 Section 003 </Text>
+            </VStack>
+            <ColorModeSwitcher justifySelf="flex-end" />
+          </HStack>
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <ClientServerGraph></ClientServerGraph>
           </VStack>
         </Grid>
       </Box>
